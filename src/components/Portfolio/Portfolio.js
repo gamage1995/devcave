7e10a6729d3acaac6211e7f1c2bc692cb345d224
charuka herath <charuka09@gmail.com>
2020-03-19 00:07:39 +0530

    designed portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from 'react'
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import PortfolioCard from '../PortfolioCard/PortfolioCard'
+import { Projects } from '../../assets/projects';
 
 import './Portfolio.css';
 
@@ -8,26 +10,35 @@ import './Portfolio.css';
 export default class Portfolio extends Component {
     render() {
         return (
-            <Row className = "POcover">
+            <Row className="POcover">
                 <Col xs></Col>
                 <Col xs="10">
                     <div>
-                    <div className = "POTop">
-                        <p className = "POheading">Our work,</p>
+                        <div className="POTop">
+                            <p className="POheading">Our work,</p>
+                        </div>
+                        <div className="POBottom"></div>
+                        <Row>
+                            {Projects.map(item => {
+                                return (
+                                    <Col xs="12" sm="12" md="6" lg="6" xl="4">
+                                        <PortfolioCard
+                                            {...item}
+                                        />
+                                    </Col>
+                                )
+                            })}
+                            {/* <Col xs="12" sm="12" md="6" lg="6" xl="4">
+                                <PortfolioCard />
+                            </Col>
+                            <Col xs="12" sm="12" md="6" lg="6" xl="4">
+                                <PortfolioCard />
+                            </Col>
+                            <Col xs="12" sm="12" md="6" lg="6" xl="4">
+                                <PortfolioCard />
+                            </Col> */}
+                        </Row>
                     </div>
-                    <div className = "POBottom"></div>
-                            <Row>
-                            <Col xs="12" sm ="12" md="6" lg ="6" xl="4">
-                                <PortfolioCard/>
-                            </Col>
-                            <Col xs="12" sm ="12" md="6" lg ="6" xl="4">
-                                <PortfolioCard/>
-                            </Col>
-                            <Col xs="12" sm ="12" md="6" lg ="6" xl="4">
-                                <PortfolioCard/>
-                            </Col>
-                            </Row>
-                </div>
                 </Col>
                 <Col xs></Col>
             </Row>
